13fe304727786c7b4f4559be221faa7018818b2b
sehee <studyingsh@gmail.com>
2021-01-31 16:12:22 +0900

    2021 KAKAO BLIND RECRUITMENT > 메뉴 리뉴얼

--- src/programmers/kakao/MenuRenewal.java
+++ src/programmers/kakao/MenuRenewal.java
@@ -1,40 +1,45 @@
 package programmers.kakao;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MenuRenewal {
 
     public static void main(String[] args) {
         MenuRenewal m = new MenuRenewal();
-        System.out.println(m.solution(new String[]{"ABC", "BCD", "CDE","DEF","EFG","FGH"}, new int[]{2,3,4}));
+        String[] result = m.solution(new String[]{
+                "XYZ", "XWY", "WXA"
+//                "QWERTYUIOP",
+//                "ASDFGHJKLP",
+//                "ZXCVBNMLPO",
+//                "ABCDEFGHIJ",
+//                "POIULKJHMN",
+//                "QAZWSXEDCR",
+//                "UHBNMKIOJL",
+//                "ZMXNALSKQW",
+//                "TPLAKSBMCN"
+        }, new int[]{2,3,5});
+
+        for (String rst:
+             result) {
+            System.out.println(rst);
+        }
     }
 
     public String[] solution(String[] orders, int[] course) {
 
-        Set<String> set = new HashSet<>();
-        Map<String,Integer> characters = new HashMap<>();
-        for(String order : orders){
-//            set.addAll(Stream.of(order.split("")).collect(Collectors.toList()));
-            for(String o : order.split("")){
-                int cnt = characters.getOrDefault(o,0);
-                characters.put(o, cnt+1);
-            }
-        }
-        set = characters.entrySet().stream().filter(e -> e.getValue() > 1).map(Map.Entry::getKey).collect(Collectors.toSet());
-
         List<String> values = new ArrayList<>();
-
-        for (int i = 0; i < course.length; i++) {
-            int count = course[i];
-            boolean[] visit = new boolean[set.size()];
-
-            Map<String, Integer> map = new HashMap<>();
-            solv(visit, orders,  "", count, 0, map, set.stream().collect(Collectors.toList()));
-
-            Optional<Map.Entry<Integer,List<Map.Entry<String,Integer>>>> optional = map.entrySet().stream()
+        // course 갯수에 따른
+        for(int count : course){
+            // 메뉴들의 모든 조합을 구하기
+            Map<String, Integer> maps = new HashMap<>();
+            for(String order : orders){
+                String[] o = order.split("");
+                Arrays.sort(o);
+                boolean[] visit = new boolean[o.length];
+                combination(o,count, visit, 0,"", maps);
+            }
+            Optional<Map.Entry<Integer,List<Map.Entry<String,Integer>>>> optional = maps.entrySet().stream()
                     .collect(Collectors.groupingBy(Map.Entry::getValue))
                     .entrySet().stream().max(Comparator.comparing(Map.Entry::getKey));
 
@@ -44,45 +49,27 @@ public class MenuRenewal {
                         .filter(e -> e.getValue() > 1)
                         .map(Map.Entry::getKey).collect(Collectors.toList());
             }
-
             values.addAll(val);
         }
 
         return values.stream().sorted().toArray(String[]::new);
     }
 
-    public void solv(boolean[] visit, String[] orders, String candidate, int count, int idx, Map<String,Integer> map,
-                     List<String> set){
-        // contains
-        if(candidate.length() == count){
-            String[] candidates = candidate.split("");
-            // check
-            for(String order : orders){
-                if(check(order,candidates)){
-                    int cnt = map.getOrDefault(candidate,0);
-                    map.put(candidate, cnt+1);
-                }
-            }
+    private void combination(String[] order, int count, boolean[] visit, int index, String result, Map<String,Integer> maps) {
+
+        if(result.length() == count){
+            int value = maps.getOrDefault(result,0);
+            maps.put(result,value+1);
         }
 
-        for(int i = idx ; i < set.size() ; i++){
+        for(int i = index ; i<order.length ; i++){
             if(!visit[i]){
                 visit[i] = true;
-                solv(visit, orders, candidate+set.get(i), count, i+1,map, set);
+                combination(order,count,visit,i+1,result+order[i],maps);
                 visit[i] = false;
             }
         }
-    }
 
-    private boolean check(String order, String[] candidates) {
-        boolean isCandidate = true;
-        for(String cand : candidates){
-            if(!order.contains(cand)){
-                isCandidate = false;
-                break;
-            }
-        }
-        return isCandidate;
     }
 
 }
