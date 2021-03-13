6fceb4e4d41587c3fa063c8a2fd80116d24c98b9
sehee <studyingsh@gmail.com>
2021-03-13 13:50:47 +0900

    이진탐색 예시

---
+++ src/programmers/search/BinarySearch.java
@@ -0,0 +1,29 @@
+package programmers.search;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,1,3,4,5};
+
+        int fin = 2;
+
+        int start = 0;
+        int end = arr.length;
+
+        int answer =-1;
+
+        while(start <= end){
+            int pivot = (start+end)/2;
+            if(arr[pivot] == fin){
+                answer = pivot;
+                break;
+            }else if(arr[pivot] > fin){
+                end = pivot -1 ;
+            }else if(arr[pivot] < fin){
+                start = pivot + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
